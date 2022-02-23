2b585f78c616c55263ed4345f1c34077d1f3e25d
sheikh-haji <64357406+sheikh-haji@users.noreply.github.com>
2022-02-23 11:04:20 +0530

    Create Longestprefixsuffix.cpp

---
+++ string/Longestprefixsuffix.cpp
@@ -0,0 +1,26 @@
+KMP ALgorithm-Concept
+https://practice.geeksforgeeks.org/problems/longest-prefix-suffix2527/1
+int lps(string s) {
+// Your code goes here
+int n = s.length();
+vector<int> lps(n, 0);
+int i=0, j=1;
+while (j < n) {
+if (s[i] == s[j]) {
+lps[j] = i+1;
+i++;
+j++;
+}
+else {
+if (i == 0) {
+lps[j] = 0;
+j++;
+}
+else {
+i = lps[i-1];
+}
+}
+}
+return lps[n-1];
+}
+
