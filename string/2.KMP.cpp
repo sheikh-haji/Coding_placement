8e23767f1070166abef5373ceb834591c55b561b
sheikh-haji <64357406+sheikh-haji@users.noreply.github.com>
2022-02-23 22:00:30 +0530

    Update and rename Longestprefixsuffix.cpp to 2.KMP.cpp
    KMP ALGORITHM

---
+++ string/2.KMP.cpp
@@ -0,0 +1,129 @@
+KMP ALgorithm-Concept
+1.KMP pattern matching
+https://practice.geeksforgeeks.org/problems/search-pattern0205/1
+
+// Prints occurrences of txt[] in pat[]
+void KMPSearch(char* pat, char* txt)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+  
+    // create lps[] that will hold the longest prefix suffix
+    // values for pattern
+    int lps[M];
+  
+    // Preprocess the pattern (calculate lps[] array)
+    computeLPSArray(pat, M, lps);
+  
+    int i = 0; // index for txt[]
+    int j = 0; // index for pat[]
+    while (i < N) {
+        if (pat[j] == txt[i]) {
+            j++;
+            i++;
+        }
+  
+        if (j == M) {
+            printf("Found pattern at index %d ", i - j);
+            j = lps[j - 1];
+        }
+  
+        // mismatch after j matches
+        else if (i < N && pat[j] != txt[i]) {
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+}
+  
+// Fills lps[] for given patttern pat[0..M-1]
+void computeLPSArray(char* pat, int M, int* lps)
+{
+    // length of the previous longest prefix suffix
+    int len = 0;
+  
+    lps[0] = 0; // lps[0] is always 0
+  
+    // the loop calculates lps[i] for i = 1 to M-1
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else // (pat[i] != pat[len])
+        {
+            // This is tricky. Consider the example.
+            // AAACAAAA and i = 7. The idea is similar
+            // to search step.
+            if (len != 0) {
+                len = lps[len - 1];
+  
+                // Also, note that we do not increment
+                // i here
+            }
+            else // if (len == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+2.Longest Prefix Suffix
+https://practice.geeksforgeeks.org/problems/longest-prefix-suffix2527/1
+
+int lps(string s) {
+// Your code goes here
+int n = s.length();
+vector<int> lps(n, 0);
+int i=0, j=1;
+while (j < n) {
+if (s[i] == s[j]) {
+lps[j] = i+1;
+i++;
+j++;
+}
+else {
+if (i == 0) {
+lps[j] = 0;
+j++;
+}
+else {
+i = lps[i-1];
+}
+}
+}
+return lps[n-1];
+}
+
+3.String Formation from substrings
+https://practice.geeksforgeeks.org/problems/string-formation-from-substring/0
+int isrepeat(string s) {
+// Your code goes here
+int n = s.length();
+vector<int> lps(n, 0);
+int i=0, j=1;
+while (j < n) {
+if (s[i] == s[j]) {
+lps[j] = i+1;
+i++;
+j++;
+}
+else {
+if (i == 0) {
+lps[j] = 0;
+j++;
+}
+else {
+i = lps[i-1];
+}
+}
+}
+return (n%(n-lps[n-1])==0)?1:0;
+}
