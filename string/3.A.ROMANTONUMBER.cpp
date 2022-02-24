6ca39ba69ed97e6bf660be1cd88ecdf0ffd8be7f
sheikh-haji <64357406+sheikh-haji@users.noreply.github.com>
2022-02-24 18:52:15 +0530

    ROMAN TO NUMBER

---
+++ string/3.A.ROMANTONUMBER.cpp
@@ -0,0 +1,27 @@
+// https://practice.geeksforgeeks.org/problems/roman-number-to-integer3201/1#
+BRUTE FORCE
+int romanToDecimal(string &str) {
+        // code here
+        unordered_map<char,int> m;
+        m.insert({'I',1});
+        m.insert({'V',5});
+        m.insert({'X',10});
+        m.insert({'L',50});
+        m.insert({'C',100});
+        m.insert({'D',500});
+        m.insert({'M',1000});
+        
+        int n=str.length();
+        int temp=m[str[n-1]];
+        for(int i=n-2;i>=0;i--){
+            
+                    if( m[str[i]]>=m[str[i+1]]){
+                    temp+=m[str[i]];}
+                    else{
+                        temp-=m[str[i]];
+                    }
+                }
+            
+        
+        return temp;
+    }
