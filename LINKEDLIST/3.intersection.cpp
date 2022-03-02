dd5eb6125441c2db19823f86c5ca138198368cd7
sheikh-haji <64357406+sheikh-haji@users.noreply.github.com>
2022-03-02 13:04:19 +0530

    BRUTE FORCE

---
+++ LINKEDLIST/3.intersection.cpp
@@ -0,0 +1,40 @@
+https://leetcode.com/submissions/detail/432666017/
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int count1=0;
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+        while(temp1!=NULL){
+            count1++;
+            temp1=temp1->next;
+        }
+        int count2=0;
+        while(temp2!=NULL){
+            count2++;
+            temp2=temp2->next;
+        }
+        int dif=abs(count1-count2);
+        temp1=headA;
+        temp2=headB;
+        if(count1>count2){
+            int i=0;
+            while(i<dif){
+                temp1=temp1->next;
+                i++;
+            }
+        }
+        else{
+            int i=0;
+            while(i<dif){
+                temp2=temp2->next;
+                i++;
+            }
+        }
+        while(temp1!=NULL and temp2!=NULL){
+            if(temp1==temp2){
+                return temp1;
+            }
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        return NULL;
+    }
