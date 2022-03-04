69087e52c28007a8ed0b5639d894c3c71ff5a888
sheikh-haji <64357406+sheikh-haji@users.noreply.github.com>
2022-03-04 09:20:39 +0530

    map + (bfs or dfs)

--- tree/4.VerticalOrderTraversalofaBinary.cpp
+++ tree/4.VerticalOrderTraversalofaBinary.cpp
@@ -1,4 +1,49 @@
 https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+Easier Implementation 
+  map<int,map<int,vector<int>>> res;
+ vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> res;
+        if (root == nullptr) {
+            return res;
+        }
+        queue<pair<int, TreeNode *>> q;
+        map<int, map<int, vector<int>>> map;
+        int x = 0;
+        q.push({0, root});
+        while (!q.empty()) {
+            int size = q.size();
+            int i = 0; 
+            while (i < size) {
+                int y = q.front().first;
+                TreeNode* curr = q.front().second;
+                q.pop();
+                if (curr->left != nullptr) {
+                    q.push({y-1, curr->left});
+                }
+                if (curr->right != nullptr) {
+                    q.push({y+1, curr->right});
+                }
+                map[y][x].push_back(curr->val);  
+                i++;
+            }
+            x++;
+        }
+        vector<int> vec;
+        for (auto i:map) {
+            for (auto j:i.second) {
+                sort(j.second.begin(), j.second.end()); 
+                for (int k=0;k<j.second.size();k++) {
+                    vec.push_back(j.second[k]);
+                }
+            }
+            res.push_back(vec);
+            vec.clear();
+        }
+        return res;
+    }
+    
+Slightly tough implementation
+map<pair<int,int>,vector<int>> res;
 
 vector<vector<int>> verticalTraversal(TreeNode* root) {
       vector<vector<int>> result;
